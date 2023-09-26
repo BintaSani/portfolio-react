d4eb46debee0789afae5b8f4b4e35ca5fb62acc3
[BintaSani] <[bintasani992@gmail.com]>
2023-09-26 11:24:01 +0100

    d

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -10,7 +10,7 @@ const Contact = () =>{
                 If you are looking for a team player, committed, detail oriented, reliable and skilled developer  with good communication skills to add to your team then look no further.
                 <br/>
                 <br/>
-                I am open to internship opportunities where i contribute to the best of my abilities, learn from inspiring developers and gain work experience.   
+                I am open to internship opportunities where i can contribute to the best of my abilities, learn from inspiring developers and gain work experience.   
             </p>
             <button className='contact' onClick={(e) => {window.open('mailto:bintasani992@gmail.com?subject=subject&body=body')}}>Gmail</button>
             <button className='contact c1' onClick={(e) => {window.open('https://www.linkedin.com/in/binta-sani-123015248/')}}>LinkedIn</button>
